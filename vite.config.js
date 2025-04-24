2ec3cb54adbabf2d959c67e9aac50a06632bccbc
Rafaela Carbelim Bandeira <134436874+rafaelaCarb@users.noreply.github.com>
2025-04-24 12:15:48 -0300

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
+export default ({
+  base: 'react-js-portfolio',
   plugins: [
     tailwindcss(),
   ],
