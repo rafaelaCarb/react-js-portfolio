33a18475c7e9d6345e56445364377a46b77989f2
Rafaela Carbelim Bandeira <134436874+rafaelaCarb@users.noreply.github.com>
2025-04-24 12:51:41 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/react-js-portfolio/' : '/',
-  plugins: [tailwindcss()],
-}));
+export default defineConfig({
+  base: '/react-js-portfolio/',
+  plugins: [react({
+    jsxRuntime: 'classic' // Adicione esta linha
+  })],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`, // Força extensão .js
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
+})
